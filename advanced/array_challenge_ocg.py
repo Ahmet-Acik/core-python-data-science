419e86ff249624d3b48483059f6984c1271e41f0
drahmetacik <a.acik@icloud.com>
2025-08-27 16:52:40 +0100

    Add implementation for removing duplicates in-place in array challenges

--- advanced/array_challenge_ocg.py
+++ advanced/array_challenge_ocg.py
@@ -14,3 +14,19 @@ def max_subarray_sum(arr):
     return max_sum
 print("Max subarray sum:", max_subarray_sum([-2,1,-3,4,-1,2,1,-5,4]))  # Output: 6
 
+
+# 2. Remove duplicates in-place and return new length
+def remove_duplicates(nums):
+    if not nums:
+        return 0
+    write_idx = 1
+    for read_idx in range(1, len(nums)):
+        if nums[read_idx] != nums[write_idx-1]:
+            nums[write_idx] = nums[read_idx]
+            write_idx += 1
+    return write_idx
+nums1 = [1,1,2,2,3]
+length = remove_duplicates(nums1)
+print("Remove duplicates:", nums1[:length], "Length:", length)  # Output: [1,2,3] Length: 3
+
+
