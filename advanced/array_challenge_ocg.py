1fb1f9a2f91c10cef9b15c08b77ffa700d798821
drahmetacik <a.acik@icloud.com>
2025-08-27 16:53:26 +0100

    Remove duplicate definition of move_zeros function in array challenge file

--- advanced/array_challenge_ocg.py
+++ advanced/array_challenge_ocg.py
@@ -42,3 +42,18 @@ def move_zeros(nums):
     return nums
 nums2 = [0,1,0,3,12]
 print("Move zeros:", move_zeros(nums2))  # Output: [1,3,12,0,0]
+
+
+# 3. Move all zeros to the end (in-place)
+def move_zeros(nums):
+    insert_pos = 0
+    for num in nums:
+        if num != 0:
+            nums[insert_pos] = num
+            insert_pos += 1
+    for i in range(insert_pos, len(nums)):
+        nums[i] = 0
+    return nums
+nums2 = [0,1,0,3,12]
+print("Move zeros:", move_zeros(nums2))  # Output: [1,3,12,0,0]
+
