baa48bb0f4cdc9d9d7ffebec5e94828f138942e8
drahmetacik <a.acik@icloud.com>
2025-08-29 13:00:04 +0100

    Add implementation for spiral order traversal of a matrix

--- advanced/array_challenge.py
+++ advanced/array_challenge.py
@@ -183,3 +183,22 @@ def find_min_rotated(nums):
             right = mid
     return nums[left]
 print("Min in rotated:", find_min_rotated([4,5,6,7,0,1,2]))  # Output: 0
+
+
+# 15. Spiral order traversal of a matrix
+def spiral_order(matrix):
+    res = []
+    while matrix:
+        res += matrix.pop(0)
+        if matrix and matrix[0]:
+            for row in matrix:
+                res.append(row.pop())
+        if matrix:
+            res += matrix.pop()[::-1]
+        if matrix and matrix[0]:
+            for row in matrix[::-1]:
+                res.append(row.pop(0))
+    return res
+print("Spiral order:", spiral_order([[1,2,3],[4,5,6],[7,8,9]]))  # Output: [1,2,3,6,9,8,7,4,5]
+
+
