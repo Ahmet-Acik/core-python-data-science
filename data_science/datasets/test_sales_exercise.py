9dd764d414f61dddcfd4392060095e645e173c6a
drahmetacik <a.acik@icloud.com>
2025-10-24 13:36:16 +0100

    Fix file path for sales dataset in test cases

--- data_science/datasets/test_sales_exercise.py
+++ data_science/datasets/test_sales_exercise.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 class TestSalesExercise(unittest.TestCase):
     def test_load(self):
-        sales = pd.read_json('sales.json')
+        sales = pd.read_json('data_science/datasets/sales.json')
         self.assertIn('revenue', sales.columns)
         self.assertEqual(sales.shape[0], 3)
+
     def test_total_revenue(self):
-        sales = pd.read_json('sales.json')
+        sales = pd.read_json('data_science/datasets/sales.json')
         self.assertEqual(sales['revenue'].sum(), 575.0)
 
 if __name__ == '__main__':
