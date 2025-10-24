df9008e0a2d56864d26ac12f24aa2aa851b01365
drahmetacik <a.acik@icloud.com>
2025-10-24 13:36:07 +0100

    Fix file path for Iris dataset in test cases

--- data_science/datasets/test_iris_exercise.py
+++ data_science/datasets/test_iris_exercise.py
@@ -5,11 +5,12 @@ from sklearn.model_selection import train_test_split
 
 class TestIrisExercise(unittest.TestCase):
     def test_load(self):
-        iris = pd.read_csv('iris.csv')
+        iris = pd.read_csv('data_science/datasets/iris.csv')
         self.assertEqual(iris.shape[1], 5)
         self.assertIn('species', iris.columns)
+
     def test_model(self):
-        iris = pd.read_csv('iris.csv')
+        iris = pd.read_csv('data_science/datasets/iris.csv')
         X = iris.drop('species', axis=1)
         y = iris['species']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
